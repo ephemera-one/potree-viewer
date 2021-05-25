0131e16b3036e3ac69bb5a692e6ebd4cfd6a8122
Vadim Smakhtin <vadim.smakhtin@habidatum.com>
2021-05-25 10:58:42 +0300

    Removed unneeded styles

--- src/components/PotreeViewer.vue
+++ src/components/PotreeViewer.vue
@@ -22,10 +22,3 @@ export default {
 </script>
 
 <style src="@/assets/css/tailwind.css" />
-<style>
-html * {
-  font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif,
-    "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
-}
-</style>
